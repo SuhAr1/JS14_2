054feec38f61469e1958b0864175918ead6c4e7c
SuhAr1 <ar.sukhomlinov@gmail.com>
2020-08-17 20:50:46 +0300

     fix: no change

--- script.js
+++ script.js
@@ -2,40 +2,37 @@
 
 let game = function() {
     let getRandomNum = function(min, max) {
-        Math.floor(Math.random() * max + min);
+    return Math.floor(Math.random() * max + min);
     };
 
     let botNum = getRandomNum(1, 100);
     
-    let answerCheck = function() {
-        prompt('Угадай число от 1 до 100');
-
-        let isNumber = function(n) {
-            return !isNaN(parseFloat(n)) && isFinite(n)
-        };
-
-        let start = function() {
-            answerCheck = prompt('Угадай число от 1 до 100');
+    let answerCheck = function(userAmount) {
         
-            while (!isNumber(answerCheck())) {
-                answerCheck() = prompt('Введи число!');
-            }
-        };
-        start();
+
+        userAmount = prompt('Угадай число от 1 до 100');
+            while (!isNumber(userAmount)) {
+            userAmount = prompt ('Введи число!')
+            };
         
-        let resultOutput = function() {
-            if (botNum > answerCheck()) {
-                console.log('Загаданное число больше');
-            } else if (botNum < answerCheck()) {
-                console.log('Загаданное число меньше');
-            } else if (botNum = answerCheck()) {
-                console.log('Поздравляю, Вы угадали!!!');
-            }
-            return botNum
-        };
-        resultOutput();
+        
+        if (botNum > userAmount) {
+            console.log('Загаданное число больше');
+        } else if (botNum < userAmount) {
+            console.log('Загаданное число меньше');
+        } else if (botNum = userAmount) {
+            console.log('Поздравляю, Вы угадали!!!');
+        }
+        return botNum
+        
         return answerCheck;
     };
-    answerCheck();
+    let userAmount = answerCheck();
 };
 game();
+
+
+        
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+};
