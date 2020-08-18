c5b678b25802af8d4be372b69baea9456f726a2c
SuhAr1 <ar.sukhomlinov@gmail.com>
2020-08-18 16:43:14 +0300

    fix fixed errors in script.js file

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 'use strict';
 
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+};  
+
 let game = function() {
     let getRandomNum = function(min, max) {
-    return Math.floor(Math.random() * max + min);
+        return Math.floor(Math.random() * max + min);
     };
 
     let botNum = getRandomNum(1, 100);
+    console.log(botNum);
+    let answerCheck = function() {
+        let userAmount = prompt('Угадай число от 1 до 100');
+        console.log(userAmount);
     
-    let answerCheck = function(userAmount) {
-        
-
-        userAmount = prompt('Угадай число от 1 до 100');
-            while (!isNumber(userAmount)) {
-            userAmount = prompt ('Введи число!')
-            };
-        
-        
-        if (botNum > userAmount) {
-            console.log('Загаданное число больше');
-        } else if (botNum < userAmount) {
-            console.log('Загаданное число меньше');
-        } else if (botNum = userAmount) {
-            console.log('Поздравляю, Вы угадали!!!');
+        if (userAmount === null) {
+            alert('Спасибо за игру!');
+            return;
         }
-        return botNum
-        
-        return answerCheck;
+
+        if (userAmount !== null) {
+            userAmount = +userAmount;
+        }
+        if (!isNumber(userAmount)) {
+            alert('Введи число!');
+            answerCheck();
+        } else if (botNum > userAmount) {
+            console.log(userAmount);
+            alert('Загаданное число больше');
+            answerCheck();
+        } else if (botNum < userAmount) {
+            alert('Загаданное число меньше');
+            answerCheck();
+        } else if (botNum === userAmount) {
+            alert('Поздравляю, Вы угадали!');
+        }    
     };
-    let userAmount = answerCheck();
+    answerCheck();
+    // console.dir(answerCheck);
 };
 game();
-
-
-        
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n)
-};
